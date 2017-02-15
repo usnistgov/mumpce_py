86e2d9aa9564b7967bacb8a5f0bbc8df7cc79e20
Sheen <dsheen@pn107154.campus.nist.gov>
2017-02-15 16:39:13 -0500

    Removing unneeded files for documentation

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from Project import *
-#from response_surface import response_surface
-#from solution import solution
--- cantera_utils/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('..')
-
-import mumpce_new as mumpce
-from initialize import ign_initialize,fls_initialize,measurement_initialize,measurement_initialize_pd
-
-
-
